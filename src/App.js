66956f77f7658ca3e8365998eeca2a22b70f9750
한재호 <hanjaeho@hanjaehoui-iMac.local>
2021-03-29 14:07:34 +0900

    learning React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+    "https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg"
+  },
+  {
+    id: 2,
+    name: "ramen",
+    image:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Original_Ramen.jpg/440px-Original_Ramen.jpg"
+  }
+];
+
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} />
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
