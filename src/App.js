4332d41bf0206b0d6911d950f9519663cdbc9412
Han Jae Ho <hanjaeho@hanjaehoui-iMac.local>
2021-03-29 17:51:30 +0900

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-import PropTypes from "prop-types";
+import React from 'react'; // This is not necessery.. maybe
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-    "https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg",
-    rating: 4.6
-  },
-  {
-    id: 2,
-    name: "ramen",
-    image:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Original_Ramen.jpg/440px-Original_Ramen.jpg",
-    rating: 5.0
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function Food({ name, picture, rating }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5</h4>
-    <img src={picture} alt={name} />
-  </div>
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food 
-          key={dish.id}
-          name={dish.name} 
-          picture={dish.image} 
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  getMovies = async () => {
+    const {
+      data: {
+        data : { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+      ); 
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }  
 }
 
 export default App;
