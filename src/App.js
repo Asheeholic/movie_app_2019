12fa298a8685e8d0753fa77c24aa075e792d6bff
한재호 <hanjaeho@hanjaehoui-iMac.local>
2021-03-29 14:28:48 +0900

    Prop-types

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import PropTypes from "prop-types";
+
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
     image:
-    "https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg"
+    "https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg",
+    rating: 4.6
   },
   {
     id: 2,
     name: "ramen",
     image:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Original_Ramen.jpg/440px-Original_Ramen.jpg"
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Original_Ramen.jpg/440px-Original_Ramen.jpg",
+    rating: 5.0
   }
 ];
 
-function Food({ name, picture }) {
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+function Food({ name, picture, rating }) {
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5</h4>
     <img src={picture} alt={name} />
   </div>
 }
@@ -24,7 +35,12 @@ function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+        <Food 
+          key={dish.id}
+          name={dish.name} 
+          picture={dish.image} 
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
